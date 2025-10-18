1d4e9afc30bdf1c5315640dfdbd544f42469bb24
roma <kopitchak2006@gmail.com>
2025-10-18 16:34:04 +0300

    feat(lab3): setup correct project structure and tooling

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'prettier', // Додаємо Prettier
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'prettier'], // Додаємо Prettier
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': 'error', // Вмикаємо Prettier як помилку ESLint
+    'react/prop-types': 'off', // Вимикаємо, якщо не використовуєш PropTypes
+  },
+};
